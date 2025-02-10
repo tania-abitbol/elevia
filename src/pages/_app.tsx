54ef6fec7a6cb3addb3d0ccdceede67735142806
Abitbol Tania <56915461+tania-abitbol@users.noreply.github.com>
2025-02-10 21:02:50 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,20 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
 
         <title>Accueil | Elevia</title>
+         <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=AW-16866771162`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16866771162');
+            `,
+          }}
+        />
       </Head>
       <GoogleAnalytics trackPageViews gaMeasurementId="G-9Z08YBERKK" />
       <Navbar />
