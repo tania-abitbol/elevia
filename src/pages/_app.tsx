4c1cf271bc5eb2cf268f8bbf0f13c4b429773b19
Abitbol Tania <56915461+tania-abitbol@users.noreply.github.com>
2025-02-11 17:57:50 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,30 +4,35 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import Head from "next/head";
+import Script from "next/script"; // Utilisation de next/script pour un chargement optimisé
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="relative">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-
         <title>Accueil | Elevia</title>
-         <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=AW-16866771162`}
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-16866771162');
-            `,
-          }}
-        />
       </Head>
+
+      {/* Chargement optimisé du script Google Tag Manager */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16866771162"
+        strategy="afterInteractive"
+      />
+      
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16866771162');
+        `}
+      </Script>
+
+      {/* Google Analytics avec suivi des pages */}
       <GoogleAnalytics trackPageViews gaMeasurementId="G-9Z08YBERKK" />
+
       <Navbar />
       <Component {...pageProps} />
       <Footer />
