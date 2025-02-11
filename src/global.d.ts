17422bc3fcfe6f0641675504f26ddae1c5ad6001
Abitbol Tania <56915461+tania-abitbol@users.noreply.github.com>
2025-02-11 17:01:45 +0100

    Update global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,6 @@
 declare global {
   interface Window {
-    gtag: Function;
+    gtag: (event: string, action: string, options: { send_to: string; value: number; currency: string }) => void;
   }
 }
 
