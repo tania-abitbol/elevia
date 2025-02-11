ad777e38fc4a1ebe6bc47dd8126ceac846bab22e
Abitbol Tania <56915461+tania-abitbol@users.noreply.github.com>
2025-02-11 17:41:43 +0100

    Update CTALiberation.tsx

--- src/components/CTALiberation.tsx
+++ src/components/CTALiberation.tsx
@@ -1,21 +1,18 @@
+import { useEffect } from "react";
+
 const CTALiberation: React.FC = () => {
-  const handleCalendlyClick = (e: React.MouseEvent) => {
-    e.preventDefault(); // Empêcher la redirection immédiate pour l'envoi de l'événement
-
-    // Log l'événement pour le débogage
-    console.log("Google Ads conversion triggered!");
-
-    // Envoi de l'événement à Google Analytics
+  const handleCalendlyClick = () => {
+    // Logique pour envoyer l'événement à Google Ads
     window.gtag("event", "conversion", {
       send_to: "AW-16866771162/xylZCOvUopwaENqB2uo-", // ID de la conversion
       value: 1.0,
       currency: "EUR",
     });
 
-    // Délai pour assurer que l'événement est capté avant la redirection
+    // Délai pour s'assurer que l'événement est bien capté avant la redirection
     setTimeout(() => {
       window.location.href = "https://calendly.com/taniaelevia/15min"; // Redirige vers Calendly
-    }, 300); // Petit délai de 300ms
+    }, 300); // Délai de 300ms
   };
 
   return (
@@ -25,15 +22,14 @@ const CTALiberation: React.FC = () => {
           Dites adieu au stress de l&apos;organisation des voyages. Profitez, on
           s&apos;occupe de tout.
         </p>
-        <a
-          href="https://calendly.com/taniaelevia/15min"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mt-4 md:mt-0 px-6 py-2 bg-white text-[var(--accent-color)] font-semibold rounded-md hover:bg-gray-200 transition"
-          onClick={handleCalendlyClick}
+
+        {/* Ici nous créons un bouton avec une div */}
+        <div
+          className="mt-4 md:mt-0 px-6 py-2 bg-white text-[var(--accent-color)] font-semibold rounded-md hover:bg-gray-200 cursor-pointer transition"
+          onClick={handleCalendlyClick} // Événement de conversion sur clic
         >
           Réserver mon appel
-        </a>
+        </div>
       </div>
     </section>
   );
