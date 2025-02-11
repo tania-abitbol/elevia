e496c19348416d12369aaf35a305ca53daaef15e
Abitbol Tania <56915461+tania-abitbol@users.noreply.github.com>
2025-02-11 18:05:43 +0100

    Update global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,7 @@
 declare global {
   interface Window {
-    gtag: (...args: any[]) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    gtag: (command: "config" | "event", id: string, params?: Record<string, any>) => void;
   }
 }
 export {};
