9c60dd47a4b862079ce9aa9d44ab433fc6c6cdc5
tania-abitbol <taniabitbol@gmail.com>
2025-02-12 13:10:41 +0100

    linkedin inte

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,13 @@
 declare global {
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    gtag: (command: "config" | "event", id: string, params?: Record<string, any>) => void;
+    gtag: (
+      command: "config" | "event",
+      id: string,
+      params?: Record<string, any>
+    ) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    lintrk: (command: "track", params: Record<string, any>) => void;
   }
 }
 export {};
