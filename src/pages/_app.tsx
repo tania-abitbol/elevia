5967a49343fb04a0e42b54908b7e4df0538884ad
Abitbol Tania <56915461+tania-abitbol@users.noreply.github.com>
2025-02-12 12:23:49 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,6 +27,30 @@ export default function App({ Component, pageProps }: AppProps) {
           gtag('config', 'AW-16866771162');
         `}
         </Script>
+         <Script id="linkedin-insight" strategy="afterInteractive">
+        {`
+          _linkedin_partner_id = "6845242";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        `}
+      </Script>
+      <Script id="linkedin-insight-script" strategy="afterInteractive">
+        {`
+          (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);
+          })(window.lintrk);
+        `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{ display: "none" }} alt="" src="https://px.ads.linkedin.com/collect/?pid=6845242&fmt=gif" />
+      </noscript>
+        
       </Head>
 
       <GoogleAnalytics trackPageViews gaMeasurementId="G-9Z08YBERKK" />
