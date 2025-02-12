e562b29ac30bca1452002108ce194c2c6f599a57
Abitbol Tania <56915461+tania-abitbol@users.noreply.github.com>
2025-02-12 12:31:38 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,46 +13,45 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <title>Accueil | Elevia</title>
 
-        <Script
+        <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-16866771162"
-          strategy="afterInteractive"
-        />
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16866771162');
+          `}
+        </script>
 
-        <Script id="google-analytics" strategy="afterInteractive">
-          {
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-16866771162');
-        }
-        </Script>
-
-         <Script id="linkedin-insight" strategy="afterInteractive">
-        {`
-          _linkedin_partner_id = "6845242";
-          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-        `}
-      </Script>
-      <Script id="linkedin-insight-script" strategy="afterInteractive">
-        {`
-          (function(l) {
-            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-            window.lintrk.q=[]}
-            var s = document.getElementsByTagName("script")[0];
-            var b = document.createElement("script");
-            b.type = "text/javascript";b.async = true;
-            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-            s.parentNode.insertBefore(b, s);
-          })(window.lintrk);
-        `}
-      </Script>
-        
+        <script>
+          {`
+            _linkedin_partner_id = "6845242";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </script>
+        <script>
+          {`
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </script>
       </Head>
-<noscript>
-<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=6845242&fmt=gif" />
-</noscript>
+
+      <noscript>
+        <img height="1" width="1" style={{ display: "none" }} alt="" src="https://px.ads.linkedin.com/collect/?pid=6845242&fmt=gif" />
+      </noscript>
+
       <GoogleAnalytics trackPageViews gaMeasurementId="G-9Z08YBERKK" />
 
       <Navbar />
