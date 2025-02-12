4e5b39d3efbe2666fcfd3d1a5f04b7fdfb8826a3
Abitbol Tania <56915461+tania-abitbol@users.noreply.github.com>
2025-02-12 12:24:27 +0100

    Update navigateToCalendly.ts

--- src/utils/navigateToCalendly.ts
+++ src/utils/navigateToCalendly.ts
@@ -6,26 +6,35 @@ export const navigateToCalendly = () => {
     window.location.href = "https://calendly.com/taniaelevia/15min";
   };
 
-  if (typeof window !== "undefined" && window.gtag) {
-    console.log(
-      "gtag est disponible, déclenchement de l'évènement conversion."
-    );
-    window.gtag("event", "conversion", {
-      send_to: "AW-16866771162/xylZCOvUopwaENqB2uo-",
-      value: 1.0,
-      currency: "EUR",
-      event_callback: () => {
-        console.log("Callback de conversion exécuté.");
+  if (typeof window !== "undefined") {
+    if (window.gtag) {
+      console.log("gtag est disponible, déclenchement de l'évènement conversion.");
+      window.gtag("event", "conversion", {
+        send_to: "AW-16866771162/xylZCOvUopwaENqB2uo-",
+        value: 1.0,
+        currency: "EUR",
+        event_callback: () => {
+          console.log("Callback de conversion exécuté.");
+          redirect();
+        },
+      });
+
+      // Fallback : si le callback ne s’exécute pas dans les 1.5 secondes, on redirige quand même
+      setTimeout(() => {
+        console.log("Timeout de fallback atteint, redirection automatique.");
         redirect();
-      },
-    });
-    // Fallback : si le callback ne s’exécute pas dans les 1.5 secondes, on redirige quand même
-    setTimeout(() => {
-      console.log("Timeout de fallback atteint, redirection automatique.");
+      }, 1500);
+    }
+
+    if (window.lintrk) {
+      console.log("LinkedIn tracking disponible, déclenchement de l'évènement conversion.");
+      window.lintrk('track', { conversion_id: 19996090 });
+    } else {
+      console.log("LinkedIn tracking non disponible, redirection immédiate.");
       redirect();
-    }, 1500);
+    }
   } else {
-    console.log("gtag non disponible, redirection immédiate.");
+    console.log("gtag et LinkedIn tracking non disponibles, redirection immédiate.");
     redirect();
   }
 };
