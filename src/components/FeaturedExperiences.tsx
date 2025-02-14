1ccbff831c2b7d90fa670ef3b6eea6c8343bcd24
tania-abitbol <taniabitbol@gmail.com>
2025-02-14 10:49:59 +0100

    fix

--- src/components/FeaturedExperiences.tsx
+++ src/components/FeaturedExperiences.tsx
@@ -4,7 +4,6 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper/modules";
-import "./carousel.css";
 
 const FeaturedExperiences: React.FC = () => {
   const experiences = [
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "../styles/carousel.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
